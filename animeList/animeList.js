40108f3ecb91e94f009d86205cf91549b0e965ec
佐藤生久美 <i-sato@solairo.co.jp>
2017-10-18 17:17:01 +0900

    アニメリスト作成

---
+++ animeList/animeList.js
@@ -0,0 +1,71 @@
+// 曜日・昼夜・タイトルを取得して、jQueryでhtmlにテキストを追加
+function addTitle() {
+    var date = document.getElementById("select").value;
+
+    var time = document.getElementsByName("time");
+    var selectedTime;
+    for (var i = 0; i < time.length; i++) {
+        if (time[i].checked) { // チェックの状態をtrue/falseで返す
+            selectedTime = time[i].value;
+            break;
+        }
+    }
+    var time2 = "noon";
+    if (selectedTime === "夜") {
+        time2 = "night";
+    }
+
+    var title = document.getElementById("id_title").value;
+
+    var targetId = date.toLowerCase() + "_" + time2;
+    var getId = document.getElementById(targetId);
+
+    var span = $("<span></span>");
+
+    if(title === "") {
+        alert("タイトルを入力してください。");
+        return false;
+    }
+    else {
+        span.text(" " + title);
+    }
+
+    var button = $("<button>X</button>");
+    button.click(function () {
+        span.remove();
+        button.remove();
+    });
+
+    $(getId).append(span);
+    $(getId).append(button);
+    document.getElementById("id_title").value = "";
+}
+
+// 季節でCSS＆季節名切り替え
+var now = new Date();
+var m = now.getMonth() + 1;
+var d = now.getDate();
+
+if(m < 10) m = "0" + m;
+if(d < 10) d = "0" + d;
+
+var date = m + "/" + d;
+var cssFile = document.getElementById("seasonCss");
+var season = $("#season");
+
+if(date >= "04/01" && date <= "06/30") {
+    cssFile.href = "spring.css";
+    season.text("春");
+}
+else if(date >= "07/01" && date <= "09/30") {
+    cssFile.href = "summer.css";
+    season.text("夏");
+}
+else if(date >= "10/01" && date <= "12/31") {
+    cssFile.href = "autumn.css";
+    season.text("秋");
+}
+else {
+    cssFile.href = "winter.css";
+    season.text("冬");
+}
